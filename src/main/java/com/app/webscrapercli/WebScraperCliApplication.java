e1f06226ee2660f1aaf71e28e159dbbc25fd414f
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-12-03 10:02:56 -0300

    chore: Add Jsoup as dependency

--- src/main/java/com/app/webscrapercli/WebScraperCliApplication.java
+++ src/main/java/com/app/webscrapercli/WebScraperCliApplication.java
@@ -1,14 +1,25 @@
 package com.app.webscrapercli;
 
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 @SpringBootApplication
 public class WebScraperCliApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebScraperCliApplication.class, args);
-		System.out.println("Hello World!");
+
+		// Jsoup test
+		try {
+			String url = "https://www.google.com";
+			Document document = Jsoup.connect(url).get();
+			System.out.println(document.html());
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
