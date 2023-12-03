ae8241221aa0b78d95acbc71369b0dd729021a53
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-12-03 09:52:17 -0300

    Initial commit

---
+++ src/main/java/com/app/webscrapercli/WebScraperCliApplication.java
@@ -0,0 +1,14 @@
+package com.app.webscrapercli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScraperCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScraperCliApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
