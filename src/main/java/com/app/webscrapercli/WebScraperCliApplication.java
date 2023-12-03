cf6bb711dc4d92ca5e7b3a828e09951034fdd312
Caio Henrique <104094613+caiohenrique-3@users.noreply.github.com>
2023-12-03 12:18:51 -0300

    feat: Add basic functionality

--- src/main/java/com/app/webscrapercli/WebScraperCliApplication.java
+++ src/main/java/com/app/webscrapercli/WebScraperCliApplication.java
@@ -1,17 +1,36 @@
 package com.app.webscrapercli;
 
-import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
-public class WebScraperCliApplication {
+import java.io.IOException;
+
+public class WebScraperCliApplication{
 	public static void main(String[] args) {
-		// Jsoup test
+		if (args.length < 2) {
+			System.out.println(
+					"[ERROR] Please provide a URL and a CSS selector!");
+			System.out.println(
+					"CORRECT FORMAT: gradle -q run --args='<url> <css-selector>'");
+			return;
+		}
+
+		String url = args[0];
+		String cssSelector = args[1];
+
 		try {
-			String url = "https://www.google.com";
-			Document document = Jsoup.connect(url).get();
-			System.out.println(document.html());
-		} catch(IOException e) {
+			Document doc = Jsoup.connect(url).get();
+			Elements elements = doc.select(cssSelector);
+
+			if (elements.isEmpty()) {
+				System.out.println("No elements match the provided CSS selector");
+			} else {
+				System.out.println("Text content of the first matching element:");
+				System.out.println(elements.first().text());
+			}
+		} catch (IOException e) {
+			System.out.println("Error fetching the URL");
 			e.printStackTrace();
 		}
 	}
